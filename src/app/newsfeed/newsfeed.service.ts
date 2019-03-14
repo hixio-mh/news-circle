188c5a443dd3b60b3e881b32c3a183e5ad87d766
Adonais0 <chuyao@umich.edu>
2019-03-14 19:16:17 -0400

    Fetch data from frontend

--- src/app/newsfeed/newsfeed.service.ts
+++ src/app/newsfeed/newsfeed.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 
 //For info on how we're sending data to django, see 'Sending data to the server' in the HttpClient section of these docs: https://angular.io/guide/http
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from '../environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -12,8 +13,7 @@ const httpOptions = {
   })
 };
 
-//Ok let's roll
-
+const APIKEY = environment.APIKEY;
 @Injectable({
     providedIn: 'root'
   })
@@ -28,28 +28,16 @@ export class NewsService {
 
     //Get articles via API
     getNewsNYT() {
-        this.httpClient.get<any>('http://127.0.0.1:8000/rest/news/').subscribe(
+        this.httpClient.get<any>(`https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=${APIKEY}`).subscribe(
             result => {
                 console.log(result); //NYT nests results in 'results ' object
-                this.news = result;
+                this.news = result.articles;
                 this.newsUpdate.next(this.news);
             }
-
-            
         );
-       
     }
 
     getNewsUpdate() {
         return this.newsUpdate.asObservable();
     }
-
-    //Add article to DB
-    addNewsNYT() {
-        this.httpClient.post<any>(this.news)
-        .pipe(
-            catchError(this.handleError('addNewsNYT', hero))
-        );
-    }
-
 }
