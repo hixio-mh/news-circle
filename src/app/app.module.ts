17f37d27707d4d5eae81cce91641996c7572df0f
fanpanzeng <zfp@umich.edu>
2019-03-27 18:03:14 -0400

    merge master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NewsService } from './newsfeed/newsfeed.service';
-<<<<<<< HEAD
 import { InvitationPageModule } from './invitation/invitation.module';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
-=======
 import { ArticlePage } from './newsfeed/newsfeed.article';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
->>>>>>> af93d57b020510be7f81c42046f064f27a79036d
 import { GroupPageModule } from './group/group.module';
 import { GroupModalComponent } from './group/group-modal/group-modal.component';
 import {GroupMemberPageModule} from './group-member/group-member.module'
@@ -41,12 +38,8 @@ import {GroupMemberPageModule} from './group-member/group-member.module'
     InAppBrowser,
     NewsService,
     SplashScreen,
-<<<<<<< HEAD
     NativeStorage,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-=======
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
->>>>>>> af93d57b020510be7f81c42046f064f27a79036d
   ],
   bootstrap: [AppComponent]
 })
