253e6dd066b11a596e8f33dcd21953f7803982a9
NDMurray <nicholasdmurray@gmail.com>
2019-04-08 08:10:54 -0400

    article page - removed middleman

--- src/app/newsfeed/newsfeed.page.ts
+++ src/app/newsfeed/newsfeed.page.ts
@@ -6,6 +6,8 @@ import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthAPIService } from '../auth/auth.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-newsfeed',
@@ -20,7 +22,7 @@ export class NewsfeedPage {
 //https://forum.ionicframework.com/t/how-to-pass-data-from-1-page-to-another-using-navigation-in-ionic-4/151060/2
 
   //Notice multiple parameters in one constructor
-  constructor(private newsService: NewsService, private router: Router,private authService: AuthAPIService ) {
+  constructor(private newsService: NewsService, private router: Router,private authService: AuthAPIService, private iab: InAppBrowser ) {
  
       this.newsfeed = newsService.news;
       this.newsService.getNews();
@@ -34,7 +36,8 @@ export class NewsfeedPage {
 
   goToArticle(article, source) {
 
-    this.router.navigate(['article',{article:article, source:source}]);
+    // this.router.navigate(['article',{article:article, source:source}]);
+    const browser = this.iab.create(article);
 
 
   }
