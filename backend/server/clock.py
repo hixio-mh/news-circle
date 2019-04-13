c9402dc2bbb647e15e575b34e5849da201f97779
NDMurray <nicholasdmurray@gmail.com>
2019-04-13 14:54:53 -0400

    fixed scheduler

--- backend/server/clock.py
+++ backend/server/clock.py
@@ -6,7 +6,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', hours=1)
 def timed_job():
 	print('Refreshing news table.')
-    print('This job is run every hour')
+	print('This job is run every hour')
 
 
 
